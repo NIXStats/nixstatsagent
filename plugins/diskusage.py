8e5372a4fbc7a3aca69fad3703d1714a1d5500c5
vfuse <vincent@nixstats.com>
2016-08-12 14:32:01 +0200

    Update diskusage.py

--- plugins/diskusage.py
+++ plugins/diskusage.py
@@ -1,14 +1,17 @@
+#!/usr/bin/env python
+
+import pickle
 import sys
 import os
 
 
-def run(config):
-    disk = {}
-    if sys.platform == "linux" or sys.platform == "linux2" or sys.platform == "darwin":
-        disk['df'] = [s.split() for s in os.popen("df -Pl").read().splitlines()]
-        disk['di'] = [s.split() for s in os.popen("df -iPl").read().splitlines()]
-    elif sys.platform == "win32":
-        c = wmi.WMI ()
-        for d in c.Win32_LogicalDisk():
-            disk['windows'] = ( d.Caption, d.FreeSpace, d.Size, d.DriveType)
-    return disk
+disk = {}
+if sys.platform == "linux" or sys.platform == "linux2" or sys.platform == "darwin":
+    disk['df'] = [s.split() for s in os.popen("df -Pl").read().splitlines()]
+    disk['di'] = [s.split() for s in os.popen("df -iPl").read().splitlines()]
+elif sys.platform == "win32":
+    c = wmi.WMI ()
+    for d in c.Win32_LogicalDisk():
+        disk['windows'] = ( d.Caption, d.FreeSpace, d.Size, d.DriveType)
+
+pickle.dump(disk, sys.stdout)
