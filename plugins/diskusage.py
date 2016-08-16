d9857ae4cc4779ebdb1eb72c08f87b11d7d62d03
vfuse <vincent@nixstats.com>
2016-08-16 07:52:06 -0700

    Updated windows support

--- plugins/diskusage.py
+++ plugins/diskusage.py
@@ -3,15 +3,26 @@
 import pickle
 import sys
 import os
-
+import psutil
 
 disk = {}
 if sys.platform == "linux" or sys.platform == "linux2" or sys.platform == "darwin":
     disk['df'] = [s.split() for s in os.popen("df -Pl").read().splitlines()]
     disk['di'] = [s.split() for s in os.popen("df -iPl").read().splitlines()]
 elif sys.platform == "win32":
-    c = wmi.WMI ()
-    for d in c.Win32_LogicalDisk():
-        disk['windows'] = ( d.Caption, d.FreeSpace, d.Size, d.DriveType)
-
+    disk = {}
+    disk['df-windows'] = []
+    for part in psutil.disk_partitions(all=False):
+        if os.name == 'nt':
+            if 'cdrom' in part.opts or part.fstype == '':
+                # skip cd-rom drives with no disk in it; they may raise
+                # ENOENT, pop-up a Windows GUI error for a non-ready
+                # partition or just hang.
+                continue
+        usage = psutil.disk_usage(part.mountpoint)
+        diskdata = {}
+        diskdata['info'] = part
+        for key in usage._fields:
+            diskdata[key] = getattr(usage, key)
+        disk['df-windows'].append(diskdata)
 pickle.dump(disk, sys.stdout)
