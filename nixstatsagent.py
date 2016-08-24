80eaf2c5edbc5216d384c875cb2f6ebf0c6b4c85
Al Nikolov <root@toor.fi.eu.org>
2016-08-24 14:01:37 +0300

    Proper inline comments in the example ini. Fixes #7. Remove redundant metrics section.

--- nixstatsagent.py
+++ nixstatsagent.py
@@ -64,7 +64,6 @@ class Agent():
         sections = [
             'agent',
             'execution',
-            'metrics',
             'data',
         ]
         config = ConfigParser.RawConfigParser(defaults)
