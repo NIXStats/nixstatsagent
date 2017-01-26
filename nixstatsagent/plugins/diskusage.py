1d07a7e47602303e6deb346ba927b47cb30a98b8
vfuse <vincent@nixstats.com>
2017-01-26 13:03:37 +0100

    Syntax

--- nixstatsagent/plugins/diskusage.py
+++ nixstatsagent/plugins/diskusage.py
@@ -17,7 +17,7 @@ class Plugin(plugins.BasePlugin):
         if sys.platform == "linux" or sys.platform == "linux2" or sys.platform == "darwin" or sys.platform == "freebsd11" or sys.platform == "freebsd10":
             disk['df'] = [s.split() for s in os.popen("df -Pl").read().splitlines()]
             disk['di'] = [s.split() for s in os.popen("df -iPl").read().splitlines()]
-        elif:
+        else:
             disk = {}
             disk['df-windows'] = []
             for part in psutil.disk_partitions(all=False):
