bf36d3270249456079be3f29d75e14c8c7161a87
vfuse-lappie <vincent@nixstats.com>
2018-03-08 14:11:16 +0100

    Monitor multiple minecraft servers

--- nixstatsagent/plugins/minecraft.py
+++ nixstatsagent/plugins/minecraft.py
@@ -11,47 +11,51 @@ class Plugin(plugins.BasePlugin):
         Fetch the amount of active and max players
         add to /etc/nixstats.ini
         [minecraft]
-        enabled=yes 
-        host=minecraft_host_or_IP
-        port=minecraft_PORT
+        enabled=yes
+        hosts=127.0.0.1:8000,127.0.0.2:8000...
         '''
 
-        try:
-            # Connect
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            host = config.get('minecraft', 'host')
-            port = int(config.get('minecraft', 'port'))
-            s.connect((host, port))
+        my_hosts = config.get('minecraft', 'hosts').split(',')
+        result = {}
+        for connection_string in my_hosts:
+            try:
+                # Connect
+                s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                hostname = connection_string.split(':')[0]
+                port = int(connection_string.split(':')[1])
+                s.connect((hostname, port))
 
-            # Send handshake + status request
-            s.send(self.pack_data("\x00\x00" + self.pack_data(host.encode('utf8')) + self.pack_port(port) + "\x01"))
-            s.send(self.pack_data("\x00"))
+                # Send handshake + status request
+                s.send(self.pack_data("\x00\x00" + self.pack_data(hostname.encode('utf8')) + self.pack_port(port) + "\x01"))
+                s.send(self.pack_data("\x00"))
 
-            # Read response
-            self.unpack_varint(s)     # Packet length
-            self.unpack_varint(s)     # Packet ID
-            l = self.unpack_varint(s) # String length
+                # Read response
+                self.unpack_varint(s)     # Packet length
+                self.unpack_varint(s)     # Packet ID
+                l = self.unpack_varint(s) # String length
 
-            d = ""
-            while len(d) < l:
-                d += s.recv(1024)
+                d = ""
+                while len(d) < l:
+                    d += s.recv(1024)
 
-            # Close our socket
-            s.close()
-        except:
-            return "Could not connect to server"
+                # Close our socket
+                s.close()
+            except:
+                pass
 
-        results = {}
+            results = {}
 
-        try:
-            players = json.loads(d.decode('utf8'))['players']
-            results['online'] = int(players['online'])
-            results['max'] = int(players['max'])
-        except:
-            results['online'] = 0
-            results['max'] = 0
+            try:
+                players = json.loads(d.decode('utf8'))['players']
+                results['online'] = int(players['online'])
+                results['max'] = int(players['max'])
+            except:
+                results['online'] = 0
+                results['max'] = 0
+            result[str(connection_string.replace('.', '-'))] = results
 
-        return results
+        return result
+
 
     def unpack_varint(self, s):
         d = 0
