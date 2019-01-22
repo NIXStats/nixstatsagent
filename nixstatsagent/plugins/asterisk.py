52c249c3f86c5101b4073a6f4f8f3f052c950c11
Joe Mordica <jmordica@gmail.com>
2019-01-21 22:01:27 -0600

    updated asterisk plugin to support an SBC

--- nixstatsagent/plugins/asterisk.py
+++ nixstatsagent/plugins/asterisk.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 import plugins
 import subprocess
 
@@ -7,12 +7,17 @@ import subprocess
 class Plugin(plugins.BasePlugin):
     __name__ = 'asterisk'
 
-    def run(self, *unused):
+    def run(self, config):
+        ip = config.get(__name__, 'sbcip')
         p = subprocess.Popen("sudo asterisk -rx 'core show calls' | grep 'active' | cut -f1 -d ' '", stdout=subprocess.PIPE, shell=True)
         p = p.communicate()[0].decode('utf-8').replace("\n", "")
         incoming = subprocess.Popen("sudo asterisk -rx 'core show channels verbose' | cut -c1-15 | grep 'pstn_' | wc -l", stdout=subprocess.PIPE, shell=True)
         incoming = incoming.communicate()[0].decode('utf-8').replace("\n", "")
-        res = { "calls": p, "incomingcalls": incoming }
+
+        devices = subprocess.Popen("sudo asterisk -rx 'sip show peers' | grep '%s' | wc -l" % (ip), stdout=subprocess.PIPE, shell=True)
+        devices = devices.communicate()[0].decode('utf-8').replace("\n", "")
+
+        res = { "calls": p, "incomingcalls": incoming, "devices": devices }
         return res
 
 if __name__ == '__main__':
