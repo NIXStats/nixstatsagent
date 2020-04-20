03303b7b66deaa27f5155522f8cb65b4d7177a7a
vfuse-lappie <vincent@nixstats.com>
2020-04-20 12:44:50 +0200

    diskstatus plugin bug fix

--- nixstatsagent/plugins/diskstatus.py
+++ nixstatsagent/plugins/diskstatus.py
@@ -12,16 +12,16 @@ class Plugin(plugins.BasePlugin):
     def run(self, config):
         '''
         Monitor nvme or smart disk status.
-        For NVME drives install nvme-cli (https://github.com/linux-nvme/nvme-cli#distro-support)
+        For NVME drives use the diskstatus-nvme plugin
         for smart status install smartmontools (apt-get/yum install smartmontools)
         This plugin requires the agent to be run under the root user.
         '''
         results = {}
 
         try:
-            devlist = subprocess.Popen('smartctl --scan', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()[0].splitlines()
+            devlist = subprocess.Popen('smartctl --scan', stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True).communicate()[0].decode().splitlines()
             smartctl = True
-        except Exception:
+        except Exception as e:
             smartctl = False
             return "Could not fetch smartctl status information"
 
@@ -33,16 +33,21 @@ class Plugin(plugins.BasePlugin):
                     smart_status = 0
                     if disk_stats[4].split(': ')[1] == 'PASSED':
                         smart_status = 1
-                    del disk_stats[:9]
                     results[disk_id] = {}
+                    start = False
                     for stats in disk_stats:
+                        if stats[0:3] == 'ID#':
+                            start = True
+                            continue
+                        if start is False:
+                            continue
                         stats = re.sub(' +', ' ', stats).strip()
                         stats = stats.split(' ')
                         if len(stats) > 9:
                             results[disk_id][stats[1].lower().replace('_celsius','')] = stats[9]
                     results[disk_id]["status"] = smart_status
-                except Exception:
-                    pass
+                except Exception as e:
+                    print(e)
         return results
 
 
--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ else:
 
 setuptools.setup(
     name='nixstatsagent',
-    version='1.2.7',
+    version='1.2.8',
     description='NixStats agent',
     long_description=readme,
     url='https://github.com/NIXStats/nixstatsagent',
