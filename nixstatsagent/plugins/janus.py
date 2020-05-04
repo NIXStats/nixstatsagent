b93d184ccecb93dbb2780a2df08d0d6fd944313f
Joe Mordica <jmordica@gmail.com>
2020-05-04 12:24:19 -0500

    fixed line 12 :)

--- nixstatsagent/plugins/janus.py
+++ nixstatsagent/plugins/janus.py
@@ -9,7 +9,7 @@ class Plugin(plugins.BasePlugin):
 
     def run(self, config):
         adminpw = config.get('janus', 'adminpw')
-        p = subprocess.Popen("curl -s -H \"Accept: application/json\" -H \"Content-type: application/json\" -X POST -d '{ \"janus\": \"list_sessions\", \"transaction\": \"324\", \"admin_secret\": \"$
+        p = subprocess.Popen("curl -s -H \"Accept: application/json\" -H \"Content-type: application/json\" -X POST -d '{ \"janus\": \"list_sessions\", \"transaction\": \"324\", \"admin_secret\": \""+adminpw+"\" }' http://localhost:7088/admin | awk 'NR>=5' | head -n -2 | wc -l", stdout=subprocess.PIPE, shell=True)
         p = p.communicate()[0].decode('utf-8').replace("\n", "")
         res = { "janus_sessions": p }
         return res
