dd57b65b6d0efd2704de593e3cc5e1c1879fcf97
Stanislav Smirnov <ssmirnov@plesk.com>
2021-09-14 19:10:34 +0700

    Create new travis key

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ else:
 
 setuptools.setup(
     name='agent360',
-    version='1.2.25',
+    version='1.2.26',
     description='360 agent',
     long_description_content_type='text/markdown',
     long_description=readme,
