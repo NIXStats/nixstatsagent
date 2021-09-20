5f5b7612e425efacf1f324d502dc5345e89bdcb6
Stanislav Smirnov <ssmirnov@plesk.com>
2021-09-20 16:15:56 +0700

    Edit deploy scripts

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ else:
 
 setuptools.setup(
     name='agent360',
-    version='1.2.26',
+    version='1.2.27',
     description='360 agent',
     long_description_content_type='text/markdown',
     long_description=readme,
