f2f44c7cab7b147b87b31fe5c164f07aadb8d482
Stanislav Smirnov <ssmirnov@plesk.com>
2021-09-20 22:58:50 +0700

    Edit deploy workflow. Bump package version to 1.2.30

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ else:
 
 setuptools.setup(
     name='agent360',
-    version='1.2.29',
+    version='1.2.30',
     description='360 agent',
     long_description_content_type='text/markdown',
     long_description=readme,
