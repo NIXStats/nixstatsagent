4e309fb9c03d6739270a885493b5d4d5356997dd
Stanislav Smirnov <ssmirnov@plesk.com>
2021-11-08 16:34:22 +0700

    TECH Bump version up to 1.2.32

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ else:
 
 setuptools.setup(
     name='agent360',
-    version='1.2.31',
+    version='1.2.32',
     description='360 agent',
     long_description_content_type='text/markdown',
     long_description=readme,
