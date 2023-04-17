53089c70d4799438712de1940a5c613a3deeb21a
mlgtcode <107129718+mlgtcode@users.noreply.github.com>
2023-04-17 11:05:28 +0200

    fix

--- agent360/plugins/bird.py
+++ agent360/plugins/bird.py
@@ -11,11 +11,11 @@ class Plugin(plugins.BasePlugin):
         Monitor status of bgp sessions using bird
         '''
         data = {}
-        data['established'] = int(os.popen('sudo birdc show proto | grep -c Established').read())
-        data['connect'] = int(os.popen('sudo birdc show proto | grep -c Connect').read())
-        data['active'] = int(os.popen('sudo birdc show proto | grep -c Active').read())
-        data['conn_reset_bp'] = int(os.popen('sudo birdc show proto | grep -c "Connection reset by peer"').read())
-        data['hold_timer'] = int(os.popen('sudo birdc show proto | grep -c "Hold timer expired").read())
+        data['established'] = int(os.popen('sudo birdc show proto | /bin/grep -c Established').read())
+        data['connect'] = int(os.popen('sudo birdc show proto | /bin/grep -c Connect').read())
+        data['active'] = int(os.popen('sudo birdc show proto | /bin/grep -c Active').read())
+        data['conn_reset_bp'] = int(os.popen('sudo birdc show proto | /bin/grep -c "Connection reset by peer"').read())
+        data['hold_timer'] = int(os.popen('sudo birdc show proto | /bin/grep -c "Hold timer expired"').read())
         return data
 
 if __name__ == '__main__':
