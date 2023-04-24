0ceb8b2ebfbab1153dca787fe11cc9593229d6b3
mlgtcode <107129718+mlgtcode@users.noreply.github.com>
2023-04-24 19:53:52 +0200

    adding configuration var

--- agent360/plugins/bind.py
+++ agent360/plugins/bind.py
@@ -13,8 +13,14 @@ class Plugin(plugins.BasePlugin):
 
         You must have the bind statistics-channels configured
         and you must have jq installed for json processing.
+        
+        Usage for /etc/agent360.ini:
+        [bind]
+        enabled = yes
+        port = 8053
         '''
-        result = os.popen('curl -j http://localhost:8053/json 2>/dev/null | jq ".qtypes * .rcodes * .nsstats"').read()
+        bport = config.get('bind', 'port')
+        result = os.popen('curl -j http://localhost:' + str(bport) + '/json 2>/dev/null | jq ".qtypes * .rcodes * .nsstats"').read()
         data=json.loads(result)
         return data
 
